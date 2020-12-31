b6af40aa4c7a4d881c91b2e9ed13514bdf46dce0
k8m-git <kouki0608.job@gmail.com>
2020-12-31 16:46:49 +0900

    TopTabNavigationの作成

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,7 +7,7 @@ import { Octicons, MaterialCommunityIcons} from '@expo/vector-icons';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 
 // If you are not familiar with React Navigation, we recommend going through the
@@ -31,6 +31,8 @@ function RootNavigator() {
     <Stack.Navigator screenOptions={{
       headerStyle: {
         backgroundColor: '#0C6157',
+        shadowOpacity: 0,
+        elevation: 0,
       },
       headerTintColor: '#fff',
       headerTitleAlign: 'left',
@@ -40,7 +42,7 @@ function RootNavigator() {
     }}>
       <Stack.Screen
         name="Root"
-        component={BottomTabNavigator}
+        component={MainTabNavigator}
         options={{
           title: "WhatsApp",
           headerRight: () => (
